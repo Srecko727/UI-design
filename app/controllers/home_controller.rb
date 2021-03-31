310e8f5102a14d85acf6a92b70e9ecf03c393b62
Srecko727 <sreckomarkovic@ufl.edu>
2021-03-30 23:42:26 -0400

    added pages to divide from

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def homepage
   end
+
+  def labourData
+  end
+
+  def crimeData
+  end
+
+  def covidData
+  end
+
+  def otherQueries
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   #get 'home/homepage'
   root 'home#homepage'
+
+  get 'home/labourData'
+  get 'home/crimeData'
+  get 'home/covidData'
+  get 'home/otherQueries'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
