532b8e7c39ad683d53092704da25de5c5e2af316
exit <jimmysoccer0927@gmail.com>
2021-04-01 18:49:57 +0800

    add oci8 gem and a helper function

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
+require 'oci8'
 module HomeHelper
+	def connect
+		#username and password needs to be edited to yours
+		con=OCI8.new('username','password','oracle.cise.ufl.edu:1521/orcl')
+		
+		cursor = con.exec("select name from country
+		fetch first 10 rows only")
+		while r = cursor.fetch()
+			p r.join(',')
+		end
+		cursor.close
+		con.logoff
+		return r
+	end
 end
