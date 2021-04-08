2b1cd36cc00e69230d96b8b7cdeefb249cdaa50d
jimmysoccer <57927853+jimmysoccer@users.noreply.github.com>
2021-04-08 15:20:41 +0800

    Update home_helper.rb

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,15 +2,13 @@ require 'oci8'
 module HomeHelper
 	def connect
 		#username and password needs to be edited to yours
-		con=OCI8.new('username','password','oracle.cise.ufl.edu:1521/orcl')
-		
-		cursor = con.exec("select name from country
-		fetch first 10 rows only")
-		while r = cursor.fetch()
-			p r.join(',')
-		end
-		cursor.close
-		con.logoff
-		return r
+		oci=OCI8.new('username','password','oracle.cise.ufl.edu:1521/orcl')
+		oci.exec('select name,code from country fetch first 10 rows only') do |record|
+				concat record.join("\b\n")
+			end
+		oci.logoff
+	end
+end
+
 	end
 end
