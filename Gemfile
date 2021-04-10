9cec6124871f40ea97c3d306f64c2a50d33a5d19
exit <jimmysoccer0927@gmail.com>
2021-04-10 21:41:26 +0800

    simple graph

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
+gem "chartkick"
+gem 'chart-js-rails'
 gem 'ruby-oci8'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3'
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,10 @@
 require 'ruby-oci8'
-
 module HomeHelper
 	def connect
 		oci=OCI8.new('hesun','092700Jimmy','oracle.cise.ufl.edu:1521/orcl')
-		oci.exec('select name from country fetch first 10 rows only') do |record|
-				puts record.join(',')
+		oci.exec('SELECT * FROM "G.AGRAWAL"."LABOR_DATA"') do |record|
+				concat record.join("\b\n")
 			end
+		oci.logoff
 	end
 end
